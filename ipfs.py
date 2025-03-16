9704ab17c24e83acddfbd66f9a1f2bc1273398f3
Runyu (Ryan) Zhang <fallacy1977@gmail.com>
2025-03-16 14:57:38 -0400

    modified:   ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -8,7 +8,7 @@ def pin_to_ipfs(data):
     json_data = json.dumps(data)
     
     # Define the IPFS API endpoint for adding data
-    ipfs_add_url = "orange-objective-tarsier-545.mypinata.cloud"
+    ipfs_add_url = "https://ipfs.infura.io:5001/api/v0/add"
     
     # Send the data to IPFS
     response = requests.post(ipfs_add_url, files={"file": json_data})
